29e8351a672d59febbb4fbda361c8aec90bf1782
Антон <toni.golovkin.89@inbox.ru>
2021-06-13 20:23:19 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/hw5/MainApp.java
@@ -0,0 +1,17 @@
+package ru.geekbrains.hw5;
+
+
+public class MainApp {
+    public static void main(String[] args) {
+            AppThread thread = new AppThread();
+            thread.runOneThread();
+            
+        }
+}
+
+
+
+
+
+
+
